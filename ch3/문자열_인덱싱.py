94da4ea83e33194b991b3acd55566610a23316de
ByeoungJoon Jeon <juni8453@naver.com>
2023-03-14 16:41:19 +0900

    문자열 인덱싱 실습

---
+++ ch3/문자열_인덱싱.py
@@ -0,0 +1,10 @@
+my_name = "My name is Tany"
+print(my_name[1])
+print(my_name[2])
+print(my_name[3])
+
+# 음수 인덱싱도 사용할 수 있다.
+print(my_name[-1]) # y
+print(my_name[-2]) # n
+print(my_name[-3]) # a
+print(my_name[-4]) # T

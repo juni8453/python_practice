5781ad78d10e06ba101e3a8938e619003d9bf623
ByeoungJoon Jeon <juni8453@naver.com>
2023-03-14 17:16:38 +0900

    리스트 인덱싱 실습

---
+++ ch4/리스트_인덱싱.py
@@ -0,0 +1,9 @@
+list = [123, 'abc', 'apple']
+print(list[0])
+print(list[1])
+print(list[2])
+
+# index() 메서드를 사용해 값을 넣어주면 해당하는 인덱스 위치를 알아낼 수 있다.
+print(list.index(123))
+print(list.index('abc'))
+print(list.index('apple'))

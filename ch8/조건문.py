56721556c5ecdeb8b196197b0f7178b9f7ac1c5d
ByeoungJoon Jeon <juni8453@naver.com>
2023-03-14 20:12:40 +0900

    while 실습

---
+++ ch8/조건문.py
@@ -0,0 +1,25 @@
+# if 조건:
+#   실행 명령1:
+#   실행 명령2:
+
+input_name = 'Tany'
+if input_name == 'Tany':
+  print('만나서 반갑습니다. ' + input_name)
+
+input_name = 'Jerry'
+if input_name == 'Tany':
+  print('만나서 반갑습니다. ' + input_name)
+
+else:
+  print('만나서 반갑습니다. ' + input_name)
+
+
+input_name = 'Tany'
+if input_name == 'Tany':
+  print('당신이 Tany 이군요.')
+
+elif input_name == 'Jun':
+  print('당신이 Jun 이군요.')
+
+else:
+  print('당신이 Hoo 군요.')

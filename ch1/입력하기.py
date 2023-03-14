d19b8836cfdc5f66fa92391ecd974196b4530f50
ByeoungJoon Jeon <juni8453@naver.com>
2023-03-14 15:27:31 +0900

    입력과 출력 실습

---
+++ ch1/입력하기.py
@@ -0,0 +1,2 @@
+age = input("나이를 입력해주세요.")
+print(age)
---
+++ ch1/출력하기.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+print('python')
+
+print(1)
+
+print(1,2,3)
+
+print(1, 2, 3)
+
+print([1,2,3])
+
+print('My name is', 'Left')

5a2305c203bfdd5666a532ef54da1cf8e21f40ab
ByeoungJoon Jeon <juni8453@naver.com>
2023-03-14 16:24:07 +0900

    문자열 기초 실습

---
+++ ch3/문자열.py
@@ -0,0 +1,21 @@
+# 쌍 따옴표, 홀 따옴표 상관 없이 가능
+my_str = "Tany"
+print(my_str)
+print(type(my_str))
+
+my_str2 = 'Tany'
+print(my_str2)
+print(type(my_str2))
+
+# 쌍 따옴표, 홀 따옴표 3개 사용 시 줄 바꿈 가능
+my_str3 = """제스퍼 
+토미
+메타
+"""
+print(my_str3)
+
+my_str4 = '''제스퍼
+토미
+메타
+'''
+print(my_str4)

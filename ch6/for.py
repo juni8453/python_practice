d735394f2a4a41f42ffa83c817e896d2bd53e18d
ByeoungJoon Jeon <juni8453@naver.com>
2023-03-21 17:14:39 +0900

    for 예제 추가

--- ch6/for.py
+++ ch6/for.py
@@ -10,3 +10,12 @@ for num in nums:
 strs = 'tomato'
 for str in strs:
   print(str)
+
+scores = [90, 85, 77, 65, 97]
+cheating_list = set([2, 4])
+
+for i in range(5):
+  if i + 1 in cheating_list:
+    continue
+  if scores[i] >= 80:
+    print(i + 1, "번 학생은 합격입니다.")

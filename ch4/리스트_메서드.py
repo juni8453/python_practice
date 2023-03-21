2793f1d83488daab791f8aec14e0506838605a7f
ByeoungJoon Jeon <juni8453@naver.com>
2023-03-21 09:16:40 +0900

    리스트 메서드 예제 추가

--- ch4/리스트_메서드.py
+++ ch4/리스트_메서드.py
@@ -17,3 +17,38 @@ print(animals.count('코알라'))
 print(len(list), end=', ')
 print(len(animals), end=', ')
 print(len('str'))
+
+# 값 추가 O(1)
+list = [1,2,3,4,5,6]
+list.append(7)
+print(list)
+
+# 특정 인덱스에 값 추가 O(N)
+list = [1,2,3,4,5,6]
+list.insert(1, 'a')
+print(list)
+
+# 내림차순 정렬 O(nlogN)
+list = [1,2,3,4,5,6]
+list.sort(reverse=True)
+print(list)
+
+# 원소 뒤집기 O(N)
+list = [1, 2, 3, 4, 5, 6]
+list.reverse()
+print(list)
+
+# 특정 값 개수 세기 O(N)
+list = [1, 2, 3, 3, 4, 5, 6]
+print(list.count(3))
+
+# 특정 값 삭제 (여러 개라면 하나만 삭제) O(N)
+list = [1, 2, 3, 3, 4, 5, 6]
+list.remove(3)
+print(list)
+
+# 특정 값 모두 삭제 시 집합 자료형(set)과 섞어서 구현하면 좋다.
+list = [1, 2, 3, 3, 4, 5, 5, 5]
+remove_set = {3, 5}
+result = [i for i in list if i not in remove_set]
+print(result)

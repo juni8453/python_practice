2234dddf47a526a1bd9f5586c0e4bc314eb5fe83
ByeoungJoon Jeon <juni8453@naver.com>
2023-03-27 16:49:25 +0900

    파이썬 문자열 정렬 1번 풀이

---
+++ programmers/문자열_정렬하기_1.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+  answer = []
+
+  for i in my_string:
+    if not i.isalpha():
+      answer.append(int(i))
+
+  answer.sort()
+  return answer
+
+print(solution(my_string="ab153"))

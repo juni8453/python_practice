08c08634ea3c9c826a603f2a1d77baa4463a1067
ByeoungJoon Jeon <juni8453@naver.com>
2023-03-30 13:14:34 +0900

    이코테 큰 수의 법칙 수정

--- algorithm/this_is_coding_test/greedy/큰_수의_법칙.py
+++ algorithm/this_is_coding_test/greedy/큰_수의_법칙.py
@@ -1,7 +1,18 @@
 n, m, k = map(int, input().split())
 data = list(map(int, input().split()))
 
-def solution(n, m, k, data):
-  pass
+answer = 0
+data.sort()
+first = data[-1]
+second = data[-2]
 
-print(solution())
+while 1:
+  for _ in range(k):
+    if m == 0: break
+    m -= 1
+    answer += first
+  if m == 0: break
+  m -= 1
+  answer += second
+
+print(answer)

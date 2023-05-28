a92cf68ebd24d05a5606fbdba682c05f7d106b12
ByeoungJoon Jeon <juni8453@naver.com>
2023-05-28 14:06:46 +0900

    이진 변환 반복하기 복습

---  problem_solving_strategy/복습/string/이진_변환_반복하기_복습.py
+++  problem_solving_strategy/복습/string/이진_변환_반복하기_복습.py
@@ -1,6 +1,4 @@
 def solution(s):
-    answer = []
-
     # x 진법 -> 10진법
     # int(<해당 진법 문자열>, <해당 진법>)
 
@@ -10,19 +8,25 @@ def solution(s):
     # 10 진법 -> x 진법
     # 따로 구현 필요
 
-    # x 의 모든 0을 제거한다.
+    call_func_count = 0
+    removed_zero = 0
 
+    while s != '1':
+        call_func_count += 1
+        s_len = len(s)
 
-    return answer
+        # x 의 모든 0을 제거한다.
+        # == 1의 개수를 세는 것과 동일
+        now_s = list(s)
+        c = now_s.count('1')
+        removed_zero += (s_len - c)
+
+        # c 를 이진 변환한다.
+        s = bin(c)[2:]
+
+    return [call_func_count, removed_zero]
 
 
 print(solution("110010101001"))
 print(solution("01110"))
-
-# 0과 1로 이루어진 어떤 문자열 x에 대한 이진 변환을 다음과 같이 정의합니다.
-#
-# x의 모든 0을 제거합니다.
-# x의 길이를 c라고 하면, x를 "c를 2진법으로 표현한 문자열"로 바꿉니다.
-# 예를 들어, x = "0111010"이라면, x에 이진 변환을 가하면 x = "0111010" -> "1111" -> "100" 이 됩니다.
-#
-# 0과 1로 이루어진 문자열 s가 매개변수로 주어집니다. s가 "1"이 될 때까지 계속해서 s에 이진 변환을 가했을 때, 이진 변환의 횟수와 변환 과정에서 제거된 모든 0의 개수를 각각 배열에 담아 return 하도록 solution 함수를 완성해주세요.
+print(solution("1111111"))

90e8cf304d2883fe6ae95c66cada5bb3fe8a9c7a
ByeoungJoon Jeon <juni8453@naver.com>
2023-06-13 14:13:04 +0900

    완주하지 못한 선수 책 풀이 추가

---  problem_solving_strategy/hash/완주하지_못한_선수.py
+++  problem_solving_strategy/hash/완주하지_못한_선수.py
@@ -12,7 +12,7 @@ def solution(participant, completion):
 
     # completion 을 순회하면서 맞는 key 값을 찾아 value 를 -1
     for c in completion:
-        if c in p_dict:
+        if c in p_dict: # 없어도 되는 로직
             p_dict[c] -= 1
 
     # p_dict 에서 value 가 1인 key 추출
@@ -25,4 +25,25 @@ def solution(participant, completion):
 
 print(solution(["leo", "kiki", "eden"], ["eden", "kiki"]))
 print(solution(["mislav", "stanko", "mislav", "ana"],
+               ["stanko", "ana", "mislav"]))
+
+
+# 책 풀이
+def solution_book(participant, completion):
+    answer = dict()
+
+    for i in participant:
+        answer[i] = answer.get(i, 0) + 1
+
+    for j in completion:
+        answer[j] -= 1
+
+    for k in answer:
+        # value 가 정수형이니 0 == False, 1 == True
+        if answer[k]:
+            return k
+
+
+print(solution_book(["leo", "kiki", "eden"], ["eden", "kiki"]))
+print(solution_book(["mislav", "stanko", "mislav", "ana"],
                ["stanko", "ana", "mislav"]))

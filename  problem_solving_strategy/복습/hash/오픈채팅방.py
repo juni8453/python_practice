88af2a3462b958d0f9deb6370021bfc00fccf75a
ByeoungJoon Jeon <juni8453@naver.com>
2023-07-18 23:28:29 +0900

    주석 삭제

---  problem_solving_strategy/복습/hash/오픈채팅방.py
+++  problem_solving_strategy/복습/hash/오픈채팅방.py
@@ -37,4 +37,3 @@ def solution(records):
 
 
 print(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]))
-# return ["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]

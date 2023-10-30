cd1280a6af894c6d0c943bc643bdd02b44f5b15a
ByeoungJoon Jeon <juni8453@naver.com>
2023-10-30 14:31:30 +0900

    인프런 카드 점수(ver 1) 풀이

---
+++ inflearn/그리디/카드_점수(ver_1).py
@@ -0,0 +1,20 @@
+def solution(nums, k):
+    answer = 0
+    n = len(nums)
+
+    # k + 1 경우를 모두 탐색
+    for i in range(k + 1):
+        sumN = 0
+        for j in range(i):
+            sumN += nums[j]
+        for j in range(n - k + i, n):
+            sumN += nums[j]
+        answer = max(answer, sumN)
+
+    return answer
+
+
+print(solution([2, 3, 7, 1, 2, 1, 5], 4))
+print(solution([1, 2, 3, 5, 6, 7, 1, 3, 9], 5))
+print(solution([1, 30, 3, 5, 6, 7], 3))
+print(solution([1, 2, 15, 3, 6, 7, 8, 9], 5))

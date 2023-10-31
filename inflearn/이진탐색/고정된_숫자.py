5ebd926ca332a7fe9d310266435408823e58508e
ByeoungJoon Jeon <juni8453@naver.com>
2023-10-31 13:18:11 +0900

    인프런 고정된 숫자 풀이

---
+++ inflearn/이진탐색/고정된_숫자.py
@@ -0,0 +1,23 @@
+def solution(nums):
+    start = 0
+    end = len(nums) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if nums[mid] == mid:
+            return nums[mid]
+
+        if nums[mid] < mid:
+            start = mid + 1
+
+        if nums[mid] > mid:
+            end = mid - 1
+
+    return -1
+
+
+print(solution([-3, -2, 0, 1, 3, 5, 8, 9, 12]))
+print(solution([-10, -5, -2, 0, 4, 6, 7, 8]))
+print(solution([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(solution([-5, -3, 0, 1, 2, 3, 4, 7]))
+print(solution([0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))

9e616876154bf16749f1d0b99ec060f26e80a831
ByeoungJoon Jeon <juni8453@naver.com>
2023-11-23 21:17:19 +0900

    프로그래머스 호텔 대실 미풀

---
+++ programmers/day/호텔_대실.py
@@ -0,0 +1,8 @@
+def solution(book_time):
+    answer = 0
+
+    # 참고) https://sanghui48.tistory.com/58
+
+    return answer
+
+print(solution([["15:00", "17:00"], ["16:40", "18:20"], ["14:20", "15:20"], ["14:10", "19:20"], ["18:20", "21:20"]]))
